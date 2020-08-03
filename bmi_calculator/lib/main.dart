13828d7669e90afd70aeab1819b3f2612f07cd8c
Hedrav Bands <hr19ut@brocku.ca>
2020-08-03 01:43:10 -0400

    Use of SingleChildScrowView, no onPressed (){} yet

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(MaterialApp(
     home: Home(),
-  )
-  );
+  ));
 }
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -14,22 +16,58 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("BMI Calculator"),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: <Widget>[
-          IconButton(
-          icon: Icon(Icons.refresh),
-          onPressed: (){},)
-        ],
-      ),
-      backgroundColor: Colors.white,
-      body: Column(
-        children: <Widget>[
-          
-        ],
-      )
+        appBar: AppBar(
+          title: Text("BMI Calculator"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child:
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Weight (Kg)",
+                    labelStyle: TextStyle(color: Colors.green),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25.0),
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: "Height (cm)",
+                    labelStyle: TextStyle(color: Colors.green),
+                  ),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25.0),
+                ),
+                Container(
+                    height: 50.0,
+                    child:
+                    RaisedButton(
+                      onPressed: () {},
+                      color: Colors.green,
+                      child: Text(
+                        "Calculate",
+                        style: TextStyle(color: Colors.white, fontSize: 25.0),
+                      ),
+                    )
+                ),
+                Text("Result Here", textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25.0),)
+              ],
+            )),
     );
   }
 }

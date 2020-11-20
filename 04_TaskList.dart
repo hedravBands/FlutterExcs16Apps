a0c7c1ab7ad36fb1425785d7b03020ff8d5799f2
Hedrav Bands <60276490+hedravBands@users.noreply.github.com>
2020-11-20 02:21:17 -0500

    Update 04_TaskList.dart
    sorting tasks by done or not, on refresh.
    snack bar improved

--- 04_TaskList.dart
+++ 04_TaskList.dart
@@ -22,16 +22,14 @@ class _HomeState extends State<Home> {
   Map<String, dynamic> _lastRemoved;
   int _lastRemovedPos;
 
-
   @override
   void initState() {
     super.initState();
 
-    _readData().then((data){
-     setState(() {
-       _toDoList = json.decode(data);
-     });
-
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
     });
   }
 
@@ -42,9 +40,29 @@ class _HomeState extends State<Home> {
       _toDoController.text = "";
       newToDo["ok"] = false;
       _toDoList.add(newToDo);
+
       _saveData();
     });
   }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((a,b){
+        if (a["ok"] && !b["ok"]) return 1;
+        else if (!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+
+      _saveData();
+
+    });
+
+    return null;
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -62,11 +80,10 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Expanded(
                   child: TextField(
-                      controller: _toDoController,
-                      decoration: InputDecoration(
-                          labelText: "New Task",
-                          labelStyle: TextStyle(color: Colors.blueAccent)
-                      ),
+                    controller: _toDoController,
+                    decoration: InputDecoration(
+                        labelText: "New Task",
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
                   ),
                 ),
                 RaisedButton(
@@ -79,10 +96,12 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-                padding: EdgeInsets.only(top: 10.0),
-                itemCount: _toDoList.length,
-                itemBuilder: buildItem),
+            child: RefreshIndicator(onRefresh: _refresh,
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem),
+            ),
           )
         ], // <Widget>[]  = children[]
       ),
@@ -90,32 +109,34 @@ class _HomeState extends State<Home> {
   }
 
   Widget buildItem(context, index) {
-      return Dismissible(
-        key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
-        background: Container(
-          color: Colors.red,
-          child: Align(
-            alignment: Alignment(-0.9, 0.0),
-            child: Icon(Icons.delete, color: Colors.white,),
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(
+            Icons.delete,
+            color: Colors.white,
           ),
         ),
-        direction: DismissDirection.startToEnd,
-        child: CheckboxListTile(
-          title: Text(_toDoList[index]["title"]),
-          value: _toDoList[index]["ok"],
-          secondary: CircleAvatar(
-            child: Icon(
-                _toDoList[index]["ok"] ? Icons.check : Icons.error),
-          ),
-          onChanged: (c){
-            setState(() {
-              _toDoList[index]["ok"] = c;
-              _saveData();
-            });
-          },
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
         ),
-        onDismissed: (direction){
+        onChanged: (c) {
           setState(() {
+            _toDoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
           _lastRemoved = Map.from(_toDoList[index]);
           _lastRemovedPos = index;
           _toDoList.removeAt(index);
@@ -123,34 +144,25 @@ class _HomeState extends State<Home> {
           _saveData();
 
           final snack = SnackBar(
-              content: Text("Task \"${_lastRemoved["title"]}\" removed!"),
+            content: Text("Task \"${_lastRemoved["title"]}\" removed!"),
             action: SnackBarAction(
-              label: "Undo",
-              onPressed: (){
-                setState(() {
-                _toDoList.insert(_lastRemovedPos, _lastRemoved);
-                _saveData();
-                });
-
-              }),
-              duration: Duration(seconds: 2),
+                label: "Undo",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: Duration(seconds: 2),
           );
-
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
-
-
-
-          });
-        },
-      );
-    }
-
-
-
+        });
+      },
+    );
+  }
 
 // functions
-
-
 
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();

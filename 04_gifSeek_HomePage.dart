db22265e7481ea132ef7f88907f2bbd06bf4c75b
Hedrav Bands <60276490+hedravBands@users.noreply.github.com>
2020-11-20 03:09:42 -0500

    Create 04_gifSeek_HomePage.dart
    initial API json received

---
+++ 04_gifSeek_HomePage.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  String _search;
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=8lge2jaOAfpICnY7nIJkUAz8Ew1FlSRG&limit=20&rating=r");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=8lge2jaOAfpICnY7nIJkUAz8Ew1FlSRG&q=$_search&limit=20&offset=$_offset&rating=r&lang=en");
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState(){
+    super.initState();
+
+    _getGifs().then((map){
+      print(map);
+    });
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

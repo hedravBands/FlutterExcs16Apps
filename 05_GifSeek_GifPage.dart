e5e98ca8cf4a0ffb90707e73e2bb14ebbd388e29
Hedrav Bands <60276490+hedravBands@users.noreply.github.com>
2020-11-21 00:23:21 -0500

    Update 05_GifSeek_GifPage.dart
    sharing Gif on longPress

--- 05_GifSeek_GifPage.dart
+++ 05_GifSeek_GifPage.dart
@@ -1,32 +1,150 @@
-import "package:flutter/material.dart";
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:gifseek/ui/gif_page.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
 import 'package:share/share.dart';
 
-class GifPage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
 
-  final Map _gifData;
+class _HomePageState extends State<HomePage> {
+  String _search;
+  int _offset = 0;
 
-  GifPage(this._gifData);
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=8lge2jaOAfpICnY7nIJkUAz8Ew1FlSRG&limit=20&rating=r");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=8lge2jaOAfpICnY7nIJkUAz8Ew1FlSRG&q=$_search&limit=19&offset=$_offset&rating=r&lang=en");
+
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getGifs().then((map) {
+      print(map);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_gifData["title"]),
         backgroundColor: Colors.black,
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.share),
-              onPressed: (){
-                Share.share(_gifData["images"]["fixed_height"]["url"]);
-              })
-        ],
+        title: Image.network(
+            "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        centerTitle: true,
       ),
       backgroundColor: Colors.black,
-      body: Center(
-        child: Image.network(_gifData["images"]["fixed_height"]["url"]),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: "Seek GIFS here!",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder()),
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+              textAlign: TextAlign.center,
+              onSubmitted: (text){
+                setState(() {
+                  _search = text;
+                  _offset = 0;
+                });
+              }
+
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+                future: _getGifs(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    // ignore: missing_return,
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        width: 200.0,
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5.0,
+                        ),
+                      );
+                    default:
+                      if (snapshot.hasError) return Container();
+                      return _createGifTable(context, snapshot);
+                  }
+                }),
+          ),
+        ],
       ),
     );
   }
+
+  //used to create the last tile as 'load more'
+  int _getCount(int count){
+    if (_search == null) {return count;}
+    else {return count + 1;}
+  }
+
+
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: _getCount(snapshot.data["pagination"]["count"]),
+        itemBuilder: (context, index) {
+          if(_search == null || index < snapshot.data["pagination"]["count"])
+            return GestureDetector(
+                child: Image.network(
+                  snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+                  height: 300.0,
+                  fit: BoxFit.cover,),
+              onTap: (){
+                Navigator.push(context, 
+                MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index])));
+              },
+              onLongPress: (){
+                Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
+              },
+            );
+          return Container(
+              child: GestureDetector(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.add, color: Colors.white, size: 70.0,),
+                      Text("Gimme more fun!",
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),)
+                    ],
+                  ),
+                onTap: (){
+                  setState(() {
+                    _offset += 19;
+                  });
+                },
+              ),
+            );
+
+
+        });
+  }
 }
-
-
